c092b3c2731f3116a730a3d8197f514e5d2f945c
Wan-Lun Wang <121719627+wlwangNCKU@users.noreply.github.com>
2026-09-08 18:35:48 +0800

    Add files via upload

--- code/fit_stone.R
+++ code/fit_stone.R
@@ -25,13 +25,10 @@ source(paste0(SPATH,"/function/FM-TMVN.na.EM.R"))
 
 Y.na = as.matrix(read.csv(paste0(SPATH,"/data/source/StoneFlakes.csv"))[,c(1,4,5,6,7)])
 sc.y = scale(Y.na)
-Y_norm <- apply(Y.na, 2, function(x) {
-  (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
-})
 stonefit = function(Y, g, seed){
   p = ncol(Y)
-  a=c(0,0,0,0,0)
-  b=c(Inf, Inf,Inf,Inf,Inf)
+  a=apply(Y,2,min,na.rm=T)
+  b=apply(Y,2,max,na.rm=T)
   set.seed(seed)
   Y.knn = kNN(Y, k = 5)[,1:p]
   set.seed(seed)
@@ -40,12 +37,12 @@ stonefit = function(Y, g, seed){
   fit_FMTMVN = MTMVN.na.EM(Y.na = Y, Y.knn=Y.knn, a=a, b=b, g=g, init.clus=km.clus, true.clus=F, tol=1e-6, max.iter=3000, per=100)
   return(list(fit_GMM = fit_GMM, fit_FMTMVN = fit_FMTMVN))
 }
-s_1 = stonefit(Y = Y.na, g=1, seed=20250523)
-s_2 = stonefit(Y = Y.na, g=2, seed=20250523)
-s_3 = stonefit(Y = Y.na, g=3, seed=20250523)
-s_4 = stonefit(Y = Y.na, g=4, seed=20250523)
-s_5 = stonefit(Y = Y.na, g=5, seed=20250523)
-s_6 = stonefit(Y = Y.na, g=6, seed=20250523)
+s_1 = stonefit(Y = sc.y, g=1, seed=20250526)
+s_2 = stonefit(Y = sc.y, g=2, seed=20250526)
+s_3 = stonefit(Y = sc.y, g=3, seed=20250526)
+s_4 = stonefit(Y = sc.y, g=4, seed=20250526)
+s_5 = stonefit(Y = sc.y, g=5, seed=20250526)
+s_6 = stonefit(Y = sc.y, g=6, seed=20250526)
 total = list()
 for (g in 1:6) {
   total[[paste0(g,"_1")]] = get(paste0("s_",g))$fit_GMM
